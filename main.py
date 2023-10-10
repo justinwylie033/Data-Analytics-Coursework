5201f358488a3432c8dde3e9221dd680f7733cc4
kristynrogan <bodfrey2195@hotmail.com>
2023-10-10 01:58:25 +0100

    data cleaning not far off

--- main.py
+++ main.py
@@ -1,48 +1,80 @@
 import pandas as pd
-from typing import Tuple
+from utils import clean_age, clean_credit_amount, correct_spelling_issues, make_lower,remove_commas_and_quotes, replace_nan_with_median
 
-def clean_data(file_path: str, log_file_path: str, output_file_path: str) -> pd.DataFrame:
-    """
-    A function to clean the dataset.
+def write_data_for_inspection(data, step):
+    filename = f'data_inspection_step_{step}.csv'
+    data.to_csv(filename, index=False)
+    print(f'Data written to {filename} for inspection.')
 
-    :param file_path: str, path to the input data file
-    :param log_file_path: str, path where the log file should be saved
-    :param output_file_path: str, path where the cleaned data should be saved
-    :return: pd.DataFrame, cleaned data
-    """
-    # A. Data Loading
-    data = pd.read_excel(file_path)
+def clean_data(data):
+    column_names = [
+        'case_no', 'checking_account', 'credit_history', 'purpose', 'credit_amount',
+        'savings', 'employment_duration', 'gender_status', 'age', 'job', 'risk'
+    ]
+    data.columns = column_names
+    
+    # Step 0: Remove commas and quotes from all columns
+    for col in data.columns:
+        print(f"Removing commas and quotes from column {col}.")
+        data[col] = remove_commas_and_quotes(data[col])
+    write_data_for_inspection(data, step=0)
+    
+    # Step 1: Checking Account Mapping
+    data['checking_account'] = data['checking_account'].map({
+        "<0'": 'Negative',
+        "0<=X<200'": 'Low',
+        ">=200": 'High',
+        "no checking'": 'None'
+    })
+    write_data_for_inspection(data, step=1)
 
-    # B. Data Cleaning
+    # Step 2: Spelling Corrections
+    for col_name, col_data in data.items():
+        if pd.api.types.is_string_dtype(col_data):
+            data[col_name] = correct_spelling_issues(col_data)
+    write_data_for_inspection(data, step=2)
 
-    ## B.1 Handle Missing Values
-    data_filled = data.fillna(data.median())
+    # # Step 3: Cleaning credit_amount
+    # data['credit_amount'] = clean_credit_amount(data['credit_amount'])
+    # write_data_for_inspection(data, step=3)
 
-    ## B.2 Handle Outliers
-    Q1 = data_filled.quantile(0.25)
-    Q3 = data_filled.quantile(0.75)
-    IQR = Q3 - Q1
-    data_no_outliers = data_filled.where((data_filled >= Q1 - 1.5*IQR) & (data_filled <= Q3 + 1.5*IQR))
+    # Step 4: Cleaning age
+    data['age'] = clean_age(data['age'])
+    write_data_for_inspection(data, step=4)
 
-    ## B.3 Check Consistency and Validity
-    # Note: Replace 'age' with the actual column name you're validating
-    valid_data = data_no_outliers[(data_no_outliers['age'] >= 18) & (data_no_outliers['age'] <= 100)]
+    # Step 5: Risk Classification
+    data['risk'] = data['risk'].apply(lambda x: "Acceptable" if x == 'good' else ("Risky" if x == 'bad' else 'Unknown'))
+    write_data_for_inspection(data, step=5)
 
-    # C. Documentation
-    log = """
-    - Replaced missing values with median of respective columns.
-    - Capped outliers beyond 1.5*IQR for all columns.
-    - Ensured age is between 18 and 100.
-    """
-    with open(log_file_path, "w") as file:
-        file.write(log)
+    # Step 6: Handling Gender and Marital Status
+    data['gender'] = data['gender_status'].apply(lambda x: 'Male' if 'male' in x else 'Female')
+    data['marital_status'] = data['gender_status'].apply(
+        lambda x: 'Single' if 'single' in x else ('Married' if 'mar' in x else ('Divorced' if 'div' in x else 'Other'))
+    )
+    data.drop(columns=['gender_status'], inplace=True)
+    write_data_for_inspection(data, step=6)
 
-    # D. Data Export
-    valid_data.to_excel(output_file_path)
+    # Step 7: Replacing NaN in numeric columns
+    numeric_cols = data.select_dtypes(include=['float64', 'int64']).columns
+    for col in numeric_cols:
+        print(f"Replacing NaN values in column {col}.")
+        data[col] = replace_nan_with_median(data[col])
+    write_data_for_inspection(data, step=7)
 
-    # Returning the cleaned data
-    return valid_data
+    return data
 
 
-if __name__ == "__main__":
-    cleaned_data = clean_data("credits.xlsx", "data_cleaning_log.txt", "cleaned_data.xlsx")
+# Example usage:
+try:
+    raw_data = pd.read_excel('credits.xlsx')
+    cleaned_data = clean_data(raw_data)
+    
+    # Set 'case_no' as the index
+    cleaned_data.set_index('case_no', inplace=True)
+    
+    print(cleaned_data.head())
+    
+    # Save to CSV with 'case_no' as the index
+    cleaned_data.to_csv('cleaned_data.csv', index=True, index_label='case_no')
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
